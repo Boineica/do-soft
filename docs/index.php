ee7a4e9339311d9c1df1a7d197330dbcc808ec06
Boineica <85923289+Boineica@users.noreply.github.com>
2021-12-05 15:16:28 +0800

    Create index.php

---
+++ docs/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Do-Soft | Do your soft job</title>
+        <meta charset="UTF-8">
+        <meta name="description" content="Free Web tutorials">
+        <meta name="keywords" content="HTML, CSS, JavaScript">
+        <meta name="author" content="John Doe">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link href="index.css">
+        <script href="index.js"></script>
+    </head>
+    <body>
+        <div id="root">
+            <div class="doger-header"></div>
+            <h1 id="title" onclick="title2()">Do-Soft | Do your soft job</h1>
+            <div class="dark-footer"></div>
+        </div>
+    </body>
+</html>
